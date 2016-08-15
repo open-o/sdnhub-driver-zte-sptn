a1c4be885c647f0dcd5c2e600d39e59f246e8534
冯洁00075937 <feng.jie2@zte.com.cn>
2016-08-15 17:21:37 +0800

    1. Remove auto-generated files 2. Add yaml file 3. move to package org.openo
    Change-Id: I79a7e8924eb357527e273bab16088139df09cb90
    Signed-off-by: Feng Jie <feng.jie2@zte.com.cn>

---
+++ src/main/java/org/openo/sdno/driver/zte/sptn/resources/L3Resource.java
@@ -0,0 +1,60 @@
+package org.openo.sdno.driver.zte.sptn.resources;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.validation.Validator;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+import org.openo.sdno.driver.zte.sptn.models.north.NL3Vpn;
+
+/**
+ * Copyright (C) 2016 ZTE, Inc. and others. All rights reserved. (ZTE)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+@Path("/sptndriver/v1/nbi-l3vpn")
+@Produces(MediaType.APPLICATION_JSON)
+public class L3Resource {
+
+  private final Validator validator;
+
+  public L3Resource(Validator validator) {
+    this.validator = validator;
+  }
+
+  @POST
+  public javax.ws.rs.core.Response createEline(NL3Vpn L3) throws URISyntaxException {
+
+    return Response.created(new
+                                URI(String.valueOf(new NL3Vpn()))).build();
+
+  }
+
+  @GET
+  public Response getEline(String ElineId) throws URISyntaxException {
+    return Response.created(new
+                                URI(String.valueOf(new NL3Vpn()))).build();
+  }
+
+  @DELETE
+  public Response deleteEline(String ElineId) throws URISyntaxException {
+    return Response.created(new
+                                URI(String.valueOf(new NL3Vpn()))).build();
+  }
+}
